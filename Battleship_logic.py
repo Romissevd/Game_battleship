898d79d930886426f5dd2a171e59c0d2b2b149d0
Roman Evdokimov <Romissevd@gmail.com>
2017-01-05 17:34:22 +0200

    произведен рефакторинг кода battleship_logic согласно PEP 8

--- Battleship_logic.py
+++ Battleship_logic.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 
+import random
+import copy
+
 """
 Игра 'Морской бой'. Модель игры - компьютер-игрок с полем 10 на 10.
 Реализованная на Python 3.
@@ -8,18 +11,16 @@
 2. Корабли не могут касаться друг друга.
 """
 
-import random  # модуль генератора случайных чисел
-import pygame
+field_game = [['0' for row in range(1, 11)] for col in range(1, 11)]  # создание игрового поля 10 на 10
+field_player = copy.deepcopy(field_game)  # полная копия игрового поля
+lst_sp_coord = []
+SHIPS = []
 
-# создание игрового поля 10 на 10
-field_game = [['0' for row in range(1, 11)] for col in range(1, 11)]
-field_player = [['0' for row in range(1, 11)] for col in range(1, 11)]
-
-
-# функия отображения поля
 def print_field(field):
+    """функия отображения поля"""
     num = 1
     print("   1 2 3 4 5 6 7 8 9 10")
+
     for column in field:
         if num < 10:
             print(num, end="  ")
@@ -29,16 +30,16 @@ def print_field(field):
         num += 1
 
 
-lst_sp_coord = []
-
-
 class Ship():
+
     def __init__(self, deck):
+
         self.deck = deck
         self.shot_deck = 1
         self.ship_sost = []
 
     def coordinate(self, field):
+        """создание кораблей на игровом поле"""
         self.sp_coord = []
 
         while True:
@@ -48,51 +49,57 @@ class Ship():
             self.column = random.randint(0, (len(field[0]) - 1))
             self.disposition = random.choice(["vertical+", "vertical-", "horizontal+", "horizontal-"])
             self.sp_coord.append((self.string, self.column))
+
             while self.value < self.deck:
                 if self.disposition == "vertical+":
-                    if self.string + self.value > 9:
+                    if self.string + self.value > (len(field[0]) - 1):
                         self.sp_coord.append((self.string - self.error, self.column))
-                        self.error = self.error + 1
+                        self.error += 1
                     else:
                         self.sp_coord.append((self.string + self.value, self.column))
-                    self.value = self.value + 1
+
                 elif self.disposition == "horizontal+":
-                    if self.column + self.value > 9:
+                    if self.column + self.value > (len(field) - 1):
                         self.sp_coord.append((self.string, self.column - self.error))
-                        self.error = self.error + 1
+                        self.error += 1
                     else:
                         self.sp_coord.append((self.string, self.column + self.value))
-                    self.value = self.value + 1
+
                 elif self.disposition == "vertical-":
                     if self.string - self.value < 0:
                         self.sp_coord.append((self.string + self.error, self.column))
-                        self.error = self.error + 1
+                        self.error += 1
                     else:
                         self.sp_coord.append((self.string - self.value, self.column))
-                    self.value = self.value + 1
+
                 elif self.disposition == "horizontal-":
                     if self.column - self.value < 0:
                         self.sp_coord.append((self.string, self.column + self.error))
-                        self.error = self.error + 1
+                        self.error += 1
                     else:
                         self.sp_coord.append((self.string, self.column - self.value))
-                    self.value = self.value + 1
-                else:
-                    print("Что-то пошло не так.")
 
-            for x, y in self.sp_coord:
-                if field[x][y] == "H" or field[x][y] == "*":
+                else:
+                    raise RuntimeError("Failed to build ships")
+
+                self.value += 1
+
+            for coord_x, coord_y in self.sp_coord:
+                if field[coord_x][coord_y] == "H" or field[coord_x][coord_y] == "*":
                     self.sp_coord = []
 
-            if len(self.sp_coord) == 0:
+            if not self.sp_coord:
                 continue
 
             for x, y in self.sp_coord:
                 field[x][y] = "H"
 
             for x, y in self.sp_coord:
-                for m, n in [(x - 1, y + 1), (x, y + 1), (x + 1, y + 1), (x - 1, y), (x + 1, y), (x - 1, y - 1),
+                for m, n in [(x - 1, y + 1), (x, y + 1),
+                             (x + 1, y + 1), (x - 1, y),
+                             (x + 1, y), (x - 1, y - 1),
                              (x, y - 1), (x + 1, y - 1)]:
+
                     if (-1) < m < 10 and (-1) < n < 10 and field[m][n] != "H":
                         field[m][n] = "*"
 
@@ -111,108 +118,88 @@ class Ship():
         else:
             return ["Мимо!"]
 
-ships = []
-def f(x):
-    ships.append(Ship(x))
 
-z = 4
-for x in range(z):
+def make_ships(deck):
+    """Создание кораблей"""
+    SHIPS.append(Ship(deck))
+
+# порядок создания кораблей и их количество, сперва созается один четырехпалубный, затем 2 трехпалубных и т.д.
+number_of_decks = 4
+for x in range(number_of_decks):
     for y in range(x+1):
-        f(z)
-    z -=1
+        make_ships(number_of_decks)
+    number_of_decks -= 1
 
-
-# ship_4_1 = Ship(4)
-# ship_3_1 = Ship(3)
-# ship_3_2 = Ship(3)
-# ship_2_1 = Ship(2)
-# ship_2_2 = Ship(2)
-# ship_2_3 = Ship(2)
-# ship_1_1 = Ship(1)
-# ship_1_2 = Ship(1)
-# ship_1_3 = Ship(1)
-# ship_1_4 = Ship(1)
-#
-# ships = [ship_4_1, ship_3_1, ship_3_2, ship_2_1, ship_2_2,
-#          ship_2_3, ship_1_1, ship_1_2, ship_1_3, ship_1_4]
-
-for ship in ships:
+for ship in SHIPS:
     ship.coordinate(field_game)
 
+
 def start_game(value):
-    st = []
-    st.append('Игра началась!')
-    st.append('Вам дается %d попыток, чтобы найти корабли.' % (value-1))
-    return st
+    """информация о начале игры"""
+    start_message = []
+    start_message.append('Игра началась!')
+    start_message.append('Вам дается %d попыток, чтобы найти корабли.' % (value-1))
+    return start_message
+
+
+def field_around_destroyed_ship(coordinat):
+    """координаты вогруг уничтоженного корабля"""
+    result_coordinat_around_destroyed_ship = []
+    for x, y in coordinat:
+        for m, n in [(x - 1, y + 1), (x, y + 1),
+                     (x + 1, y + 1), (x - 1, y),
+                     (x + 1, y), (x - 1, y - 1),
+                     (x, y - 1), (x + 1, y - 1)]:
+
+            if (-1) < m < len(field_player) and (-1) < n < len(field_player[0]) and field_player[m][n] != "I":
+                field_player[m][n] = "*"
+                result_coordinat_around_destroyed_ship.append((m, n))
+
+    return result_coordinat_around_destroyed_ship
+
 
 def game(pl_str, pl_col, value, total_shot_ship):
-
-    text = []
+    """Ход игрового процесса"""
+    text_message = []
+    player_string = pl_str + 1
+    player_column = pl_col + 1
 
     if total_shot_ship == 10:
-        print("Поздравляем вы выиграли!")
-        #break
-        # координаты вводимые пользователем с проверкой ввода типа данных
-    try:
-        player_string = pl_str + 1
-        player_column = pl_col + 1
+        text_message.append("Поздравляем вы выиграли!")
+        return [text_message, []]
 
-    except ValueError:
-        print("Вы должны вводить цифры!!!")
-        #continue
-
-    if not (0 < player_string < 11) or not (0 < player_column < 11):
-        print("Эй, таких координат нет!!!")
-        print("У вас осталось - %s попыток" % (value - 1))
-        #continue
-    elif value > 1:  # количество попыток
-        text.append("У вас осталось - %d попыток" % (value-1))
-        print(text)
+    if value > 1:
+        text_message.append("У вас осталось - %d попыток" % (value-1))
         lst_sost = []
-        coord_for_ship_in_visual_field = []
-        for shot_ship in ships:
+
+        for shot_ship in SHIPS:
             sost = shot_ship.shot((player_string - 1), (player_column - 1))
             lst_sost.append(sost)
+
         for l in lst_sost:
-
             if len(l) > 1:
-                for (x, y) in l[1]:
-                    field_player[x][y] = "I"
-                print("Уничтожил!!!")
-                text.append("Уничтожил!!!")
-                for x, y in l[1]:
-
-                    for m, n in [(x - 1, y + 1), (x, y + 1), (x + 1, y + 1), (x - 1, y), (x + 1, y), (x - 1, y - 1),
-                                 (x, y - 1), (x + 1, y - 1)]:
-                        if (-1) < m < 10 and (-1) < n < 10 and field_player[m][n] != "I":
-                            field_player[m][n] = "*"
-                            coord_for_ship_in_visual_field.append((m, n))
+                for (coord_hit_ship_x, coord_hit_ship_y) in l[1]:
+                    field_player[coord_hit_ship_x][coord_hit_ship_y] = "I"
+                text_message.append(l[0])
                 total_shot_ship += 1
-                return [text, coord_for_ship_in_visual_field]
+                return [text_message, field_around_destroyed_ship(l[1])]
         else:
             if field_player[(player_string - 1)][(player_column - 1)] == "I" or \
                             field_player[(player_string - 1)][(player_column - 1)] == "H":
-                text.append("Корабль уже подбит. Будь внимателен!!!")
+                text_message.append("Корабль уже подбит. Будь внимателен!!!")
             elif ["Подбил!"] in lst_sost:
                 field_player[(player_string - 1)][(player_column - 1)] = "H"
-                print("Подбил!!!")
-                text.append("Подбил!!!")
+                text_message.append("Подбил!!!")
             else:
                 if field_player[(player_string - 1)][(player_column - 1)] == "0":
                     field_player[(player_string - 1)][(player_column - 1)] = "X"
                 elif field_player[(player_string - 1)][(player_column - 1)] == "*":
-                    text.append("Сюда можно не стрелять. Тут кораблей не будет!")
+                    text_message.append("Сюда можно не стрелять. Тут кораблей не будет!")
                 elif field_player[(player_string - 1)][(player_column - 1)] == "X":
-                    text.append("Сюда уже стрелял!!!")
-                    value += 1
-                #print("Мимо!!!")
-                text.append("Мимо!!!")
-        #print_field(field_player)
-        return [text, coord_for_ship_in_visual_field]
-    else:
-        text.append("К сожалению вы проиграли!!!")
-        print("К сожалению вы проиграли!!!")
-        print("Расположение кораблей было такое:")
-        print_field(field_game)
-        return [text, []]
+                    text_message.append("Сюда уже стрелял!!!")
+                text_message.append("Мимо!!!")
 
+        return [text_message, []]
+    else:
+        text_message.append("К сожалению вы проиграли!!!")
+        return [text_message, []]
